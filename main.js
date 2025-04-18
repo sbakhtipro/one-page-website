42452916d6544fba7ec5ef77b5d6e8975a39e72c
sbakhtipro <sarah.bakhti.pro@gmail.com>
2025-04-18 19:41:24 +0200

    finalisation js carousel

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
 const leftArrow = document.querySelector('.carousel__left-arrow')
 const rightArrow = document.querySelector('.carousel__right-arrow')
 const carouselSlider = document.querySelector('.carousel__slider')
+const slides = document.querySelectorAll('.carousel__element')
 const slide = document.querySelector('.carousel__element')
 const slideWidth = slide.offsetWidth
 
 let currentIndex = 0
 
-
 leftArrow.addEventListener('click', () => {
-    currentIndex-=1
-    carouselSlider.style.transform = `translateX(${currentIndex * slideWidth}px)`
+    if (currentIndex>0) {
+        currentIndex--
+        carouselSlider.style.transform = `translateX(-${currentIndex * slideWidth}px)`
+    }
 })
 
 rightArrow.addEventListener('click', () => {
-    currentIndex+=1
-    carouselSlider.style.transform = `translateX(-${currentIndex * slideWidth}px)`
+    currentIndex++
+    if (currentIndex>=0 && currentIndex<slides.length) {
+        carouselSlider.style.transform = `translateX(-${currentIndex * slideWidth}px)`
+    }
+    else {
+        carouselSlider.style.transform = `translateX(0)`
+        currentIndex = 0
+    }
 })
